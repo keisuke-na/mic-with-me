24b21bc45aea076a71a41de99993699c23d18579
Keisuke Nagamori <nagamorikeisuke@gmail.com>
2022-05-17 16:32:24 +0900

    軽微な修正

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,43 +3,55 @@ import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  const videoRef = useRef<HTMLVideoElement>(null)
-  const [onMicroPhone, setOnMicroPhone] = useState<boolean>(false)
+  const mediaRef = useRef<HTMLVideoElement>(null)
   const [onCamera, setOnCamera] = useState<boolean>(false)
-  const [stream, setStream] = useState<MediaStream | null>(null)
+  const [onMicroPhone, setOnMicroPhone] = useState<boolean>(false)
+  const [asSpearker, setAsSpearker] = useState<boolean>(false)
 
-  // for Camera
   useEffect(() => {
-    if (onCamera || onMicroPhone) { 
-      startUserMediaStream(videoRef)
+    if(onCamera || onMicroPhone) {
+      initUserMediaStream(mediaRef)
+      startUserMediaStream(mediaRef)
+    }else {
+      initUserMediaStream(mediaRef)
     }
-
-    // 2つのToggle Button を on->off に切り替えたときの処理
-    if (!onCamera && !onMicroPhone && stream) {
-      resetUserMediaStream(videoRef)
-    }
-  }, [onCamera, onMicroPhone])
-
+  }, [onCamera ,onMicroPhone])
+  
+  useEffect(() => {
+    mediaRef.current!.muted = (asSpearker) ? false : true
+  }, [asSpearker])
+  
   const startUserMediaStream = async (mediaRef: RefObject<HTMLVideoElement>) => {
-    if (stream) resetUserMediaStream(mediaRef)
-
     const mediaStream = await navigator.mediaDevices.getUserMedia({ video: onCamera, audio: onMicroPhone })
     mediaRef.current!.srcObject = mediaStream
-    mediaRef.current!.volume = (onCamera) ? 0.0 : 1.0
-    mediaRef.current!.muted = (onCamera) ? true : false
-    setStream(mediaStream)
+    mediaRef.current!.volume = 1.0
   }
 
-  // 古い Stream のトラッカーを停止させる処理 (HTML要素のstreamの解除だけではカメラは停止しない)
-  const resetUserMediaStream = (mediaRef: RefObject<HTMLVideoElement>) => {
-    mediaRef.current!.srcObject = null
-
-    if (stream!.getVideoTracks()[0]) stream!.getVideoTracks()[0].stop()
-    if (stream!.getAudioTracks()[0]) stream!.getAudioTracks()[0].stop()
+  const initUserMediaStream = (mediaRef: RefObject<HTMLVideoElement>) => {
+    const mediaStream = mediaRef.current!.srcObject as MediaStream
+    if (mediaStream) {
+      // 古いトラッカーを停止させ初期化する処理 (HTML要素のstreamの解除だけではgetしたUserMediaは停止しない)
+      if (mediaStream.getVideoTracks()[0]) mediaStream.getVideoTracks()[0].stop()
+      if (mediaStream.getAudioTracks()[0]) mediaStream.getAudioTracks()[0].stop()
+      mediaRef.current!.srcObject = null
+    }
   }
 
   return (
     <div className="App">
+      <div className="form-check form-switch">
+        <input
+          className="form-check-input appearance-none w-9 -ml-10 rounded-full float-left h-5 align-top bg-white bg-no-repeat bg-contain bg-gray-300 
+            focus:outline-none cursor-pointer shadow-sm"
+          type="checkbox"
+          role="switch"
+          id="cameraCheckChecked"
+          checked={asSpearker}
+          onChange={() => { setAsSpearker(!asSpearker) }}
+        />
+        <label className="form-check-label inline-block text-gray-800 float-left" htmlFor="cameraCheckChecked">AsSpeaker</label>
+      </div>
+      <br />
       <div className="form-check form-switch">
         <input
           className="form-check-input appearance-none w-9 -ml-10 rounded-full float-left h-5 align-top bg-white bg-no-repeat bg-contain bg-gray-300 
@@ -66,7 +78,7 @@ function App() {
         <label className="form-check-label inline-block text-gray-800 float-left" htmlFor="microphoneCheckChecked">Microphone</label>
       </div>
       <br />
-      <video id="video_local" width="320" height="240" style={{ border: "1px solid black" }} ref={videoRef} autoPlay></video>
+      <video id="video_local" width="320" height="240" style={{ border: "1px solid black" }} ref={mediaRef} autoPlay></video>
     </div>
   )
 }
